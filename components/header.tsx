94645a93ef42f257a2b6de4b3814126aefe23a3b
quang-khuong-duy-le <nathanle2711@gmail.com>
2024-01-04 13:24:10 -0500

    Create header search input

--- components/header.tsx
+++ components/header.tsx
@@ -2,11 +2,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { ThemeToggler } from './theme-toggler'
+import SearchInput from './search-input'
 
 const Header = () => {
   return (
-    <header>
-        <Link href="/">
+    <header className='fixed w-full z-20 top-0 flex items-center justify-between p-5 bg-gradient-to-t from-gray-200/0 via-gray-900/25 to-gray-900'>
+        <Link href="/" className='mr-10'>
             <Image 
                 src="https://links.papareact.com/a943ae"
                 alt="Disney logo"
@@ -17,6 +18,7 @@ const Header = () => {
         </Link>
 
         <div className="flex space-x-2">
+            <SearchInput />
             <ThemeToggler />
         </div>
     </header>
