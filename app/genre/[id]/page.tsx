d096656e2b42630ebde8d6bfd3a8ff2b39203431
quang-khuong-duy-le <nathanle2711@gmail.com>
2024-01-06 15:58:48 -0500

    Load movies for genre page

--- app/genre/[id]/page.tsx
+++ app/genre/[id]/page.tsx
@@ -1,3 +1,6 @@
+import MoviesCarousel from "@/components/movies-carousel";
+import { getDiscoverMovies, getSearchedMovies } from "@/lib/getMovies";
+
 async function GenrePage({
   params: { id },
   searchParams: { genre },
@@ -7,13 +10,17 @@ async function GenrePage({
     genre: string;
   };
 }) {
+  const movies = await getDiscoverMovies(id);
+
   return (
     <div className="max-w-7xl mx-auto">
       <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
         <h1 className="text-6xl font-bold px-10">Results for {genre}</h1>
+
+        <MoviesCarousel title="Genre" movies={movies} isVertical />
       </div>
     </div>
-  );
+  )
 }
 
 export default GenrePage;
